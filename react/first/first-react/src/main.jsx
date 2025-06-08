da34cff036f4e07e06a0bbd67e5b35cb16786bcd
Akshatsah04 <iamakshatsah@gmail.com>
2025-06-08 15:21:32 +0530

    learing about export in react

--- react/first/first-react/src/main.jsx
+++ react/first/first-react/src/main.jsx
@@ -2,16 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import MainContain from './text.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
 
-function MainContain(){
-  return <h1>react is great</h1>
-}
+
 
 const root =createRoot(document.querySelector("#root"))
 
@@ -20,8 +19,10 @@ const reactElement = <h2>hello world 2</h2>
 // root.render(<h1>hello world</h1>)
 // root.render(reactElement)
 
+
 root.render(
   <div>
     <MainContain />
   </div>
+  // MainContain()
 )
---
+++ react/first/first-react/src/text.jsx
@@ -0,0 +1,4 @@
+export default function MainContain(){
+    return (<h1>react is great</h1>)
+  
+  }
